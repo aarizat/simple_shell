d5815879b979a0874e909d227c020e4d62edd21e
poix-code <1996@holbertonschool.com>
2020-08-18 21:30:58 -0500

    Define the environment variable

--- functions.h
+++ functions.h
@@ -38,7 +38,7 @@ typedef struct list_env
 	struct list_env *next;
 
 } env_t;
-
+extern char **environ;
 char *get_commands(void);
 list_t *_add_node_at_end(list_t **head, const char *str);
 int _strlen(char *s);
